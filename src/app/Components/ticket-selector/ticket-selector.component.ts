c5abcfb3194f6ba706e7232f61145b509521bbfe
amitkuzi <amitkuzi@gmail.com>
2024-04-07 13:50:35 +0300

    add bootstrap 5

--- src/app/Components/ticket-selector/ticket-selector.component.ts
+++ src/app/Components/ticket-selector/ticket-selector.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-ticket-selector',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule
+  ],
   templateUrl: './ticket-selector.component.html',
   styleUrl: './ticket-selector.component.css'
 })
